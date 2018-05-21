db1d5174bd44991ad3a5f71739baa85a705fb76f
ediaz <ediaz@celmedia.com>
2018-05-21 11:00:35 -0500

    creacion de tablas y clases y modelos

---
+++ crud/crud/Clases/Empleado.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crud.Clases
+{
+    class Empleado
+    {
+        public int EmpleadoId { get; set; }
+        public string Apellidos { get; set; }
+        public string Nombre { get; set; }
+        public string Dni { get; set; }
+        public string Genero { get; set; }
+        public string EstadoCivil { get; set; }
+        public string Direccion { get; set; }
+        public string DistritoId { get; set; }
+
+        public Empleado()
+        { 
+        }
+
+        public Empleado(string _apellidos, string _nombre, string _dni,string _genero,
+                string _estado_civil,string _direccion, string _distrito_id    
+            )
+        {
+            this.Apellidos = _apellidos;
+            this.Nombre = _nombre;
+            this.Dni = _dni;
+            this.Genero = _genero;
+            this.EstadoCivil = _estado_civil;
+            this.Direccion = _direccion;
+            this.DistritoId = _distrito_id;
+        }
+
+        public Empleado(int _empleado_id)
+        {
+            this.EmpleadoId = _empleado_id;
+        }
+    }
+}
---
+++ crud/crud/Clases/Telefono.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crud.Clases
+{
+    class Telefono
+    {
+
+        public int TelefonoId { get; set; }
+        public string Operador { get; set; }
+        public string Numero { get; set; }
+        public string Empleado_id { get; set; }
+
+        public Telefono()
+        {
+        }
+
+        public Telefono(string _operador,string _numero,string _empleado_id)
+        {
+            this.Operador = _operador;
+            this.Numero = _numero;
+            this.Empleado_id = _empleado_id;
+        }
+
+        public Telefono(int _telefono_id)
+        {
+            this.TelefonoId = _telefono_id;
+        }
+      
+    }
+
+}
